00f48a35d5a7d6039c28b396d31e9f323faa1dfe
MarcuseDemarchi <demarchimarcus2004@gmail.com>
2026-06-21 20:25:20 -0300

    fix(api)_: ajusta end-points da api

--- app/api/sales.py
+++ app/api/sales.py
@@ -1,23 +1,16 @@
-from typing import List
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from .. import crud, schemas
 from ..database import get_db
 
-router = APIRouter(prefix="/sales", tags=["Sales"])
+router = APIRouter(tags=["Purchases"])
 
 
-@router.post("/checkout", response_model=schemas.SaleOut)
-def checkout_sale(sale_in: schemas.SaleCreate, db: Session = Depends(get_db)):
+@router.post("/compras", response_model=schemas.SaleOut)
+def purchase_ticket(purchase_in: schemas.PurchaseCreate, db: Session = Depends(get_db)):
     try:
-        sale = crud.create_sale(db, sale_in)
+        sale = crud.create_purchase(db, purchase_in)
     except ValueError as error:
         raise HTTPException(status_code=400, detail=str(error))
     return sale
-
-
-@router.get("/catalog", response_model=List[schemas.EventOut])
-def event_catalog(db: Session = Depends(get_db)):
-    return crud.list_events(db)
